9e01cc335110b59a19fedaeae6f615200002c25c
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-22 15:29:55 -0500

    fixed sliders

--- public/main.js
+++ public/main.js
@@ -1,13 +1,25 @@
 const qs = s => document.querySelector(s)
 const qsa = s => document.querySelectorAll(s)
 
-const main = () => {
+const update = () => {
   const hue = qs('input[name=hue]').value
   const saturation = qs('input[name=saturation]').value
   const lightness = qs('input[name=lightness]').value
   const alpha = qs('input[name=alpha]').value
   const hsla = `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`
   qs('h2').textContent = hsla
+  qs('.picker figure').style.backgroundColor = hsla
+}
+const main = () => {
+  const inputs = qsa('.picker input')
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', update)
+  }
+  /*qs('input[name=hue]').addEventListener('input', update)
+  qs('input[name=saturation]').addEventListener('input', update)
+  qs('input[name=lightness]').addEventListener('input', update)
+  qs('input[name=alpha]').addEventListener('input', update)*/
+  update()
 }
 
 document.addEventListener('DOMContentLoaded', main)
