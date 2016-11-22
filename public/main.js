27f5b05811589c453286277bb8a85fff5e5ca521
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-11-22 12:24:12 -0500

    started JS

--- public/main.js
+++ public/main.js
@@ -1,5 +1,13 @@
+const qs = s => document.querySelector(s)
+const qsa = s => document.querySelectorAll(s)
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  const hue = qs('input[name=hue]').value
+  const saturation = qs('input[name=saturation]').value
+  const lightness = qs('input[name=lightness]').value
+  const alpha = qs('input[name=alpha]').value
+  const hsla = `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`
+  qs('h2').textContent = hsla
 }
 
 document.addEventListener('DOMContentLoaded', main)
